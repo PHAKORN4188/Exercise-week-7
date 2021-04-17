f06876c8113e05c1cc66d3baf37011f2f8adf219
Phakorn A L Kong <18DDT19F1061@student.ptss.edu.my>
2021-04-17 13:44:55 +0800

    Add project files.

---
+++ Exercise week 7/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstbox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(308, 155)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 60)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstbox
+        '
+        Me.lstbox.FormattingEnabled = True
+        Me.lstbox.ItemHeight = 20
+        Me.lstbox.Location = New System.Drawing.Point(283, 230)
+        Me.lstbox.Name = "lstbox"
+        Me.lstbox.Size = New System.Drawing.Size(202, 144)
+        Me.lstbox.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstbox)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstbox As ListBox
+End Class
---
+++ Exercise week 7/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim strDayName(6) As String
+        strDayName(0) = "Sunday"
+        strDayName(1) = "Monday"
+        strDayName(2) = "Tuesday"
+        strDayName(3) = "Wendnesday"
+        strDayName(4) = "Thrusday"
+        strDayName(5) = "Friday"
+        strDayName(6) = "Saturday"
+
+        Dim K As Integer
+        For K = 0 To 6
+            lstbox.Items.Add(strDayName(K))
+        Next
+
+    End Sub
+End Class
